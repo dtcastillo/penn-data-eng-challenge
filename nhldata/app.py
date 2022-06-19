587fe61fc86cd62210bc3468988d90bc0079ae3a
Daniel Castillo <dt.castillo20@gmail.com>
2022-06-18 19:56:44 -0700

    working solution with yml jobs

--- nhldata/app.py
+++ nhldata/app.py
@@ -9,82 +9,79 @@ automomously.  So feel free to add anywhere (not hinted, this is where we see yo
     * messaging for monitoring or troubleshooting
     * anything else you think is necessary to have for restful nights
 '''
-import logging
-from pathlib import Path
-from datetime import datetime
-from dataclasses import dataclass
-import boto3
-import requests
-import pandas as pd
-from botocore.config import Config
-from dateutil.parser import parse as dateparse
 
-logging.basicConfig(level=logging.INFO)
-LOG = logging.getLogger(__name__)
+from io import BytesIO
+from typing import List
+from ratelimit import limits, RateLimitException
+from backoff import on_exception, expo
+from concurrent.futures import ThreadPoolExecutor
+
+from botocore.config import Config
+from datetime import datetime, timedelta, timezone
+from dataclasses import dataclass
+from requests.exceptions import HTTPError, ConnectTimeout, Timeout
+import pandas as pd
+import requests
+import boto3
+
+import argparse
+import logging
+import yaml
+import os
+
+logging.basicConfig(level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+JOBS_DIR = './nhldata/jobs'
+API_CALL_LIMIT = 100
+API_PERIOD_LIMIT = 10 # Seconds
+API_MAX_RETRIES = 5
+API_LIMIT_EXCEPTIONS = (RateLimitException, HTTPError, ConnectTimeout, Timeout)
 
 class NHLApi:
-    SCHEMA_HOST = "https://statsapi.web.nhl.com/"
-    VERSION_PREFIX = "api/v1"
-
-    def __init__(self, base=None):
-        self.base = base if base else f'{self.SCHEMA_HOST}/{self.VERSION_PREFIX}'
-
+    def __init__(self, api_host, api_version):
+        self.base = '{}/{}'.format(api_host, api_version)
 
     def schedule(self, start_date: datetime, end_date: datetime) -> dict:
-        ''' 
-        returns a dict tree structure that is like
-            "dates": [ 
-                {
-                    " #.. meta info, one for each requested date ",
-                    "games": [
-                        { #.. game info },
-                        ...
-                    ]
-                },
-                ...
-            ]
-        '''
-        return self._get(self._url('schedule'), {'startDate': start_date.strftime('%Y-%m-%d'), 'endDate': end_date.strftime('%Y-%m-%d')})
+        """
+        Fetches all nhl games between start_date and end_date from schedule endpoint
 
-    def boxscore(self, game_id):
-        '''
-        returns a dict tree structure that is like
-           "teams": {
-                "home": {
-                    " #.. other meta ",
-                    "players": {
-                        $player_id: {
-                            "person": {
-                                "id": $int,
-                                "fullName": $string,
-                                #-- other info
-                                "currentTeam": {
-                                    "name": $string,
-                                    #-- other info
-                                },
-                                "stats": {
-                                    "skaterStats": {
-                                        "assists": $int,
-                                        "goals": $int,
-                                        #-- other status
-                                    }
-                                    #-- ignore "goalieStats"
-                                }
-                            }
-                        },
-                        #...
-                    }
-                },
-                "away": {
-                    #... same as "home" 
-                }
-            }
+        :param start_date
+        :param end_date
+        :return: returns games json response as dict
+        """
+        start_date = start_date.strftime('%Y-%m-%d')
+        end_date = end_date.strftime('%Y-%m-%d')
+        
+        logger.info('Fetching nhl games from {} to {}...'.format(start_date, end_date))
+        
+        url = self._url('schedule')
+        params = {'startDate': start_date, 'endDate': end_date}
+        response = self._get(url, params)
 
-            See tests/resources/boxscore.json for a real example response
-        '''
+        logger.info('Successfully fetched nhl games from {} to {}'.format(start_date, end_date))
+        
+        return response
+
+    def boxscore(self, game_id) -> dict:
+        """
+        Fetches box score stats and other metadata from boxscore endpoint for specific game_id
+
+        :param game_id
+        :return: returns boxscore json response as dict
+        """
+
+        logger.info('Fetching boxscore for game {}...'.format(game_id))
+        
         url = self._url(f'game/{game_id}/boxscore')
-        return self._get(url)
+        response = self._get(url)
+        
+        logger.info('Successfully fetched boxscore for game {}...'.format(game_id))
 
+        return response
+
+    @on_exception(expo, API_LIMIT_EXCEPTIONS, max_tries=API_MAX_RETRIES)
+    @limits(calls=API_CALL_LIMIT, period=API_PERIOD_LIMIT)
     def _get(self, url, params=None):
         response = requests.get(url, params=params)
         response.raise_for_status()
@@ -95,52 +92,278 @@ class NHLApi:
 
 @dataclass
 class StorageKey:
-    # TODO what propertie are needed to partition?
-    gameid: str
+    game_id: str
 
     def key(self):
         ''' renders the s3 key for the given set of properties '''
-        # TODO use the properties to return the s3 key
-        return f'{self.gameid}.csv'
+        return f'{self.game_id}.csv'
 
-class Storage():
-    def __init__(self, dest_bucket, s3_client):
-        self._s3_client = s3_client
-        self.bucket = dest_bucket
+class Storage:
+    def __init__(self, endpoint_url, sig_version='s3v4', num_retries=5):
+        self.endpoint_url = endpoint_url
+        self.sig_version = sig_version
+        self.num_retries = num_retries
+        
+        config = Config(signature_version = self.sig_version)
+        self._s3_client = boto3.client('s3', config = config, endpoint_url = endpoint_url)
 
-    def store_game(self, key: StorageKey, game_data) -> bool:
-        self._s3_client.put_object(Bucket=self.bucket, Key=key.key(), Body=game_data)
-        return True
+    def store_game(self, bucket, key: StorageKey, game_data: dict) -> None:
+        """
+        Uploads file object of game data to s3 bucket using key
+        
+        :param bucket: bucket to upload to
+        :param key: s3 key for file
+        :game_data bytes file object of game data
+        """
 
-class Crawler():
-    def __init__(self, api: NHLApi, storage: Storage):
-        self.api = api
-        self.storage = storage
+        for _ in range(self.num_retries):
+            try:
+                logger.info('Uploading file to s3: {}/{}'.format(bucket, key.key()))
+                self._s3_client.put_object(Bucket=bucket, Key=key.key(), Body=game_data)
+                logger.info('Successfully uploaded file to s3: {}/{}'.format(bucket, key.key()))
+                return
+            except Exception as exp:
+                logger.error('Error uploading file: {}. Retrying...'.format(exp))
+                continue
+        
+        logger.error('Failed to upload file to s3: {}/{}'.format(bucket, key.key()))
+        raise exp
 
-    def crawl(self, startDate: datetime, endDate: datetime) -> None:
-	# NOTE the data direct from the API is not quite what we want. Its nested in a way we don't want
-	#      so here we are looking for your ability to gently massage a data set. 
-        #TODO error handling
-        #TODO get games for dates
-        #TODO for each game get all player stats: schedule -> date -> teams.[home|away] -> $playerId: player_object (see boxscore above)
-        #TODO ignore goalies (players with "goalieStats")
-        #TODO output to S3 should be a csv that matches the schema of utils/create_games_stats 
-                 
+class Crawler:
+    def __init__(self, **kwargs):   
+        self.player_fields = kwargs['fields']['player']
+        self.stats_fields = kwargs['fields']['skater_stats']
+        self.player_prefix = kwargs['prefixes']['player']
+        self.stats_prefix = kwargs['prefixes']['stats']
+
+        self.api = NHLApi(api_host = kwargs['api_host'],
+                          api_version = kwargs['api_version'])
+        
+        self.storage = Storage(os.environ.get('S3_ENDPOINT_URL'))
+
+    def get_games(self, start_date, end_date) -> List:
+        """
+        Fetches all nhl games played (game ids/pks) between start_date and end_date 
+        
+        :param start_date
+        :param end_date
+        :return: list of game ids/pks
+        """
+        
+        # Fetch nhl games from schedule endpoint
+        sched = self.api.schedule(start_date, end_date)
+        dates = sched['dates']
+
+        # For each date, grab games played on that date,
+        # append to full games list
+        game_pks = []
+        for date in dates:
+            games_on_date = [x['gamePk'] for x in date['games']]
+            game_pks += games_on_date
+        
+        num_games = len(game_pks)
+        logger.info('Found {} total games between {} and {}'.format(num_games, start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')))
+        
+        return game_pks
+
+    def _gen_player_fields(self):
+        """
+        Generates full field names for player fields using player prefix
+
+        :return: list of full player fields names
+        """
+        player_fields = [self.player_prefix + x for x in self.player_fields]
+        return player_fields
+
+    def _gen_stats_fields(self):
+        """
+        Generates full field names for stats fields using stats prefix
+
+        :return: list of full stats fields names
+        """
+        stats_fields = [self.stats_prefix + x for x in self.stats_fields]
+        return stats_fields
+
+    def _gen_fields(self):
+        """
+        Generates all full field names using prefixes
+
+        :return: list of all full field names
+        """
+
+        p_fields = self._gen_player_fields()
+        s_fields = self._gen_stats_fields()
+        return p_fields + s_fields
+
+    def _convert_to_df(self, players, fields, side, max_level):
+        """
+        Converts semi-structered dict to dataframe with fields of interest
+
+        :param players: dict of nested players fields
+        :param fields: list of fields to grab
+        :param side: home or away
+        :param level: nested level of deepest fields
+        :return: df 
+        """
+        try:
+            logger.info('Converting data to df...')
+            # Flatten json up to max_level
+            df = pd.json_normalize(players, max_level = max_level)
+            
+            # Only grab fields + rename
+            df = df[fields]
+            df = df.rename(columns=lambda col: col.replace('.', '_'))
+            df = df.rename(columns=lambda col: 'player_' + col)
+
+            # Add home or away
+            df['side'] = side
+            
+            logger.info('Successfully converted {} data to df'.format(side))
+            return df
+        
+        except Exception as exp:
+            logger.error('Failed to convert to df with error {}'.format(str(exp)))
+            raise exp
+
+    def _df_to_fileobj(self, df, encoding='utf-8'):
+        """
+        Converts df to csv bytes file object in memory
+        
+        :param df: dataframe
+        :return: csv file object
+        """
+        try:
+            df.fillna('', inplace=True)
+            logger.info('Converting df to file object...')
+            
+            csv_bytes = df.to_csv(index=False).encode(encoding)
+            file_obj = BytesIO(csv_bytes)
+            
+            logger.info('Successfully converted df to file object...')
+            return file_obj
+        except Exception as exp:
+            logger.error('Failed to convert df to file object with error: {}'.format(str(exp)))
+            raise exp
+
+    def game_fetch_upload(self, game_pk, fields, max_level, bucket):
+        """
+        Fetches nhl game data for specific game id/pk from api, grabs only 
+        fields of interest and uploads to s3
+        
+        :param game_pk: game id
+        :param fields: list of fields to grab
+        :max_level: nested level of deepest fields
+        :bucket: s3 bucket to upload to
+        """
+        try:
+            box_score = self.api.boxscore(game_pk)
+
+            # Get home and away players data
+            away_players = list(box_score['teams']['away']['players'].values())
+            home_players = list(box_score['teams']['home']['players'].values())
+            
+            # Convert json to df
+            away_df = self._convert_to_df(away_players, fields, side='away', max_level=max_level)
+            home_df = self._convert_to_df(home_players, fields, side='home', max_level=max_level)
+            
+            # Combine home and away players data
+            df = pd.concat([away_df, home_df])
+
+            # Add game id and insert timestamp
+            df['game_id'] = game_pk
+            df['insert_timestamp'] = datetime.now(timezone.utc)
+            
+            # Convert df to csv file object
+            file_obj = self._df_to_fileobj(df)
+
+            # Upload to s3
+            key = StorageKey(game_pk)
+            self.storage.store_game(bucket, key, file_obj)
+            self.success_cnt += 1
+        except:
+            pass
+
+    def crawl(self, bucket, start_date, end_date, max_level=3, max_workers=3) -> None:
+        """
+        Crawls all nhl game data between start_date and end_date and uploads to s3 in
+        individual files. Multi-threaded (thread per game).
+        
+        :param bucket: s3 bucket to upload to
+        :param start_date
+        :param end_date
+        :max_level: nested level of deepest fields
+        :max_workers: number of threads
+        """
+        game_pks = self.get_games(start_date, end_date)
+        fields = self._gen_fields()
+
+        self.success_cnt = 0
+        with ThreadPoolExecutor(max_workers = max_workers) as executor:
+            logger.info('Starting ThreadPoolExecutor with {} workers...'.format(max_workers))
+            for game_pk in game_pks:
+                executor.map(self.game_fetch_upload(game_pk, fields, max_level, bucket))
+        
+        logger.info('Succesfully fetched and uploaded {} out of {} games'.format(self.success_cnt, len(game_pks)))
+        logger.info('Completed job from {} to {}'.format(start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')))
+
+        # If success count doesn't match games returned from api, 
+        # raise exception
+        if self.success_cnt != len(game_pks):
+            fail_cnt = len(game_pks) - self.success_cnt
+            logger.error('{} games failed to fetch and upload'.format(fail_cnt))
+            raise Exception('Game ids returned != game ids uploaded')
+
+class CrawlerJob:
+    def __init__(self, job_config):
+        self.job_config = job_config
+        self.job_config['backfill'] = self.job_config.get('backfill', False)
+        
+        self.crawler = Crawler(fields = job_config['fields'],
+                               prefixes = job_config['prefixes'],
+                               api_host = job_config['api_host'],
+                               api_version = job_config['api_version'])
+    
+    def run(self):
+        """
+        Runs a crawl job using a job yml
+        """
+        
+        # If backfill job, grab dates defined in job yml
+        if self.job_config['backfill']:
+            logger.info('Starting backfill job...')
+            self.job_config['start_date'] = datetime.strptime(self.job_config['start_date'], '%Y-%m-%d')
+            self.job_config['end_date'] = datetime.strptime(self.job_config['end_date'], '%Y-%m-%d')
+            
+            self.crawler.crawl(self.job_config['s3_bucket'],
+                               self.job_config['start_date'], 
+                               self.job_config['end_date'],
+                               self.job_config['max_level'],
+                               self.job_config['max_workers'])
+        
+        # Otherwise, must be incremental job, 
+        # use lookback days for dynamic dates
+        else:
+            curr_date = datetime.today()
+            lookback_date = datetime.today() - timedelta(days=self.job_config['lookback_days'])
+            
+            logger.info('Starting incrental job...')
+            self.crawler.crawl(self.job_config['s3_bucket'],
+                               lookback_date, 
+                               curr_date,
+                               self.job_config['max_level'])
+
 def main():
-    import os
-    import argparse
     parser = argparse.ArgumentParser(description='NHL Stats crawler')
-    # TODO what arguments are needed to make this thing run,  if any?
-    args = parser.parse_args()
+    parser.add_argument('--job_yml', default='backfill_job.yml')
 
-    dest_bucket = os.environ.get('DEST_BUCKET', 'output')
-    startDate = # TODO get this however but should be like datetime(2020,8,4)
-    endDate =   # TODO get this however but should be like datetime(2020,8,5)
-    api = NHLApi()
-    s3client = boto3.client('s3', config=Config(signature_version='s3v4'), endpoint_url=os.environ.get('S3_ENDPOINT_URL'))
-    storage = Storage(dest_bucket, s3client)
-    crawler = Crawler(api, storage)
-    crawler.crawl(startDate, endDate)
+    args = parser.parse_args()
+    job_yml_path = '{}/{}'.format(JOBS_DIR, args.job_yml)
+
+    config_file = open(job_yml_path, 'r')
+    job_yml = yaml.safe_load(config_file)
+    
+    crawler_job = CrawlerJob(job_yml)
+    crawler_job.run()
 
 if __name__ == '__main__':
     main()
